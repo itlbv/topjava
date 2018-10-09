2f6c11a9b64b75349fcc2e201a48a3b93ca2bb3d
itlbv <artem.v.fokin@gmail.com>
2018-10-09 12:43:11 +0200

    Fix from code review

--- src/main/java/ru/javawebinar/topjava/dao/MealDao.java
+++ src/main/java/ru/javawebinar/topjava/dao/MealDao.java
@@ -1,20 +1,16 @@
 package ru.javawebinar.topjava.dao;
 
 import ru.javawebinar.topjava.model.Meal;
-import ru.javawebinar.topjava.model.MealWithExceed;
-
-import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.util.List;
 
 public interface MealDao {
-    void add(LocalDateTime dateTime, String description, int calories);
+    void add(Meal meal);
 
-    void update(int id, LocalDateTime dateTime, String description, int calories);
+    Meal update(Meal meal);
 
     void delete(int id);
 
     Meal getById(int id);
 
-    List<MealWithExceed> getAll(LocalTime startTime, LocalTime endTime, int caloriesPerDay);
+    List<Meal> getAll();
 }
